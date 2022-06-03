547736cfd4f2fdfb34f586dbd7c466f420322bdb
Lekan <olalekancrown11@gmail.com>
2022-06-03 16:12:05 -0400

    Working rapper api

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+  '21 savage':{
+    'age': 29,
+    'birthName': 'Shéyaa Bin Abraham-Joseph',
+    'birthLocation': 'London, UK'
+  },
+  'chance the rapper':{
+    'age': 29,
+    'birthName': 'Chancelor bron',
+    'birthLocation': 'Chicago'
+  },
+  'unknown':{
+    'age': 0,
+    'birthName': 'unknown',
+    'birthLocation': 'unknown'
+  }
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (req, res) => {
+  const rapperName = req.params.name.toLowerCase();
+  if (rappers[rapperName]) {
+    res.json(rappers[rapperName])
+  } else {
+    res.json(rappers['unknown'])
+  }
+  
+})
+
+app.listen(PORT, () => {
+  console.log(`the server is running on port ${PORT}!`);
+})
